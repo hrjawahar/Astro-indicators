48d22c17efb48a3731b38c4e0a534eea930266fd
hrjawahar <hrjawahar@yahoo.com>
2026-07-18 12:36:20 +0530

    Delete build-engine.sh

--- build-engine.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Bundle the TypeScript engine (with zod) into an ESM module for Cloudflare Pages Functions.
-./node_modules/.bin/esbuild engine/run-all.ts --bundle --format=esm --platform=neutral \
-  --outfile=site/functions/api/engine-bundle.js --minify-whitespace
